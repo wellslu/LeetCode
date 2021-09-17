4e2f3e4caf89ec82054e99cc8985b20e2a682a31
wellslu <34208509+wellslu@users.noreply.github.com>
2021-09-17 09:15:43 +0800

    Delete Remove_Element.py

--- Remove_Element.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        if len(nums) == 0:
-            return 0
-        else:
-            while val in nums:
-                nums.remove(val)
-            return len(nums)
