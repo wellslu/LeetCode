69e2e9062c715f2b8ee6c63986c5c5ea223be745
wellslu <34208509+wellslu@users.noreply.github.com>
2021-09-17 09:15:22 +0800

    Delete Search_Insert_Position.py

--- Search_Insert_Position.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        l = len(nums)
-        for i in range(l):
-            if nums[i] == target:
-                return i
-            elif nums[i] > target:
-                return i
-        return i+1
