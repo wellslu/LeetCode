102fdcd96aa693ea4d0800e5ff7eb917d4de7a74
wellslu <34208509+wellslu@users.noreply.github.com>
2021-10-20 20:14:56 +0800

    Update Combination_Sum.py

--- medium/Combination_Sum.py
+++ medium/Combination_Sum.py
@@ -19,3 +19,24 @@ class Solution:
         for digit in candidates:
             self.candidate_sum([digit], target)
         return self.answer
+
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         answer = []
+#         candidates = sorted(candidates)
+#         def candidate_sum(candidates: List[int], combinations: List[int], target: int) -> None:
+#             target = target - combinations[-1]
+#             if target == 0:
+#                 if sorted(combinations) not in answer:
+#                     answer.append(sorted(combinations))
+#             elif target < 0:
+#                 pass
+#             else:
+#                 for digit in candidates:
+#                     if digit > target:
+#                         break
+#                     candidate_sum(candidates, combinations+[digit], target)
+#         for index in range(len(candidates)):
+#             candidate_sum(candidates[index:], [candidates[index]], target)
+#         return answer
