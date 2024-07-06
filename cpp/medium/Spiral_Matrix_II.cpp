52ccbf15993b37b1fc7a093833d90791bc390cd0
Wei-Chung (Wells) Lu <34208509+wellslu@users.noreply.github.com>
2024-07-06 22:54:18 +0800

    Update Spiral_Matrix_II.cpp

--- cpp/medium/Spiral_Matrix_II.cpp
+++ cpp/medium/Spiral_Matrix_II.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
+        // init 0 vector[n][n]
         vector<vector<int>> answer(n, vector<int>(n));
-        // vector<vector<int>> answer;
-        // for (int i = 0; i < n; i++) {
-        //     vector<int> v = {};
-        //     for (int j = 0; j < n; j++) {
-        //         v.push_back(3*i+j);
-        //     }
-        //     answer.push_back(v);
-        // }
 
         int num = 1;
         int i = 0, r = 0;
         int count = n*n+1;
-        while(true) {
+        for(int r = 0; r <= n / 2; r++) {
             for (i = 0+r; i <= n-r-1; i++) {
-                // cout << i << "," << r << "->" << num << endl;
                 answer[r][i] = num;
                 num++;
             }
-            if (num == count) break;
             num--;
-            for (i = 0+r; i <= n-r-1; i++) {
-                // cout << n-1-r << "," << i << "->" << num << endl;
+            for (i = 0+r; i < n-r-1; i++) {
                 answer[i][n-1-r] = num;
                 num++;
             }
-            if (num == count) break;
-            num--;
             for (int i = n-r-1; i > r; i--) {
-                // cout << i << "," << n-1-r << "->" << num << endl;
                 answer[n-1-r][i] = num;
                 num++;
             }
-            if (num == count) break;
             for (int i = n-r-1; i >= 1+r; i--) {
-                // cout << r << "," << i << "->" << num << endl;
                 answer[i][r] = num;
                 num++;
             }
-            if (num == count) break;
-            r++;
         }
         return answer;
     }
