537b907b7bdd490503eda2847acec842823cb024
Wei-Chung (Wells) Lu <34208509+wellslu@users.noreply.github.com>
2024-07-17 09:47:36 -0500

    Update Search_in_Rotated_Sorted_Array_II.cpp

--- cpp/medium/Search_in_Rotated_Sorted_Array_II.cpp
+++ cpp/medium/Search_in_Rotated_Sorted_Array_II.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        if (target >= nums[0]) {
-            for(int i = 0; i < nums.size(); ++i) {
-                if (target == nums[i]) return true;
-            }
+        if (nums.size() == 1) {
+            if (target == nums[0]) return true;
+            else return false;
         }
-        else {
-            for(int i = nums.size()-1; i >= 0; --i) {
-                if (target == nums[i]) return true;
-            }
-        }
+        return find_t(nums, target, 0, nums.size()-1);
+    }
+    bool find_t(vector<int>& nums, int target, int i, int j) {
+        if (target == nums[i] || target == nums[(j+i)/2] || target == nums[j]) return true;
+        else if (j-i <= 1) return false;
+        else if (target > nums[i] && target > nums[(j+i)/2]) return find_t(nums, target, i+1, j-1);
+        else if (target < nums[i] && target > nums[(j+i)/2]) return find_t(nums, target, (j+i)/2, j);
+        else if (target > nums[i] && target < nums[(j+i)/2]) return find_t(nums, target, i, (j+i)/2);
+        else if (target < nums[i] && target < nums[(j+i)/2]) return find_t(nums, target, i+1, j-1);
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool search(vector<int>& nums, int target) {
+//         if (target >= nums[0]) {
+//             for(int i = 0; i < nums.size(); ++i) {
+//                 if (target == nums[i]) return true;
+//             }
+//         }
+//         else {
+//             for(int i = nums.size()-1; i >= 0; --i) {
+//                 if (target == nums[i]) return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
