f38622dccc5aa172a66a056bf45394f81e6dccb4
Wei-Chung (Wells) Lu <34208509+wellslu@users.noreply.github.com>
2024-07-19 03:12:22 -0500

    Create Subsets_II.cpp

---
+++ cpp/medium/Subsets_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> answer;
+        vector<int> combination;
+        sort(nums.begin(), nums.end());
+        dfs(nums, combination, answer);
+        return answer;
+    }
+    void dfs(vector<int> nums, vector<int>& combination, vector<vector<int>>& answer) {
+        answer.push_back(combination);
+        set<int> set_num(nums.begin(), nums.end());
+        for (auto &i:set_num){
+            combination.push_back(i);
+            vector<int> sub_num(find(nums.begin(), nums.end(), i)+1, nums.end());
+            dfs(sub_num, combination, answer);
+            combination.pop_back();
+        }
+    }
+};
