3a43e305da003cd8b7cc0e61674be841899545c5
Wei-Chung (Wells) Lu <34208509+wellslu@users.noreply.github.com>
2024-08-13 09:54:32 -0500

    Create Populating_Next_Right_Pointers_in_Each_Node_II.cpp

---
+++ cpp/medium/Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        vector<Node*> queue{root};
+        next_level(queue);
+        return root;
+    }
+    void next_level(vector<Node*>& queue) {
+        Node* node = nullptr;
+        vector<Node*> next_queue;
+        for (auto q:queue) {
+            if (q && q->left != nullptr) {
+                if (node == nullptr) node = q->left;
+                else {
+                    node->next = q->left;
+                    node = node->next;
+                }
+                next_queue.push_back(q->left);
+            }
+            if (q && q->right != nullptr) {
+                if (node == nullptr) node = q->right;
+                else {
+                    node->next = q->right;
+                    node = node->next;
+                }
+                next_queue.push_back(q->right);
+            }
+        }
+        if (next_queue.size() != 0) next_level(next_queue);
+    }
+};
